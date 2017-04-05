7cb88296cfeadd956b7c95a336eed2191090e2f0
Duilio D'Ottavio <duilio.dottavio@gmail.com>
2017-04-05 19:45:11 +0200

    main come da testo esercizio

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -7,13 +7,13 @@ public class Esercizio3 {
 		Rettangolo r2 = new Rettangolo(3,16);
 		Rettangolo r3 = new Rettangolo(14,86);
 		
-		System.out.println("R1 Area: " + r1.area() + " Perimetro: " + r1.perimetro());
-		System.out.println("R2 Area: " + r2.area() + " Perimetro: " + r2.perimetro());
-		System.out.println("R3 Area: " + r3.area() + " Perimetro: " + r3.perimetro());
+		System.out.println("Somma Area: " + (r1.area() + r2.area() + r3.area()));
+		System.out.println("Somma Perimetro: " + (r1.perimetro() + r2.perimetro() + r3.perimetro()));
 		
 		r1.ridimensiona(34, 54);
 		
-		System.out.println("R1 Area: " + r1.area() + " Perimetro: " + r1.perimetro());
+		System.out.println("Somma Area: " + (r1.area() + r2.area() + r3.area()));
+		System.out.println("Somma Perimetro: " + (r1.perimetro() + r2.perimetro() + r3.perimetro()));
 
 
 		
